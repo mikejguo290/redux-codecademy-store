0eaa5993470ea35176c40ad118709f63a459adb1
mikejguo290 <mikejguo290@gmail.com>
2021-07-31 18:00:14 +0100

    corrected import path of currencyFilterReducer.

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,7 @@
 import { createStore,  combineReducers } from "redux";
 // Import the slice reducers here.
 import {cartReducer} from '../features/cart/cartSlice';
-import {currencyFilterReducer} from '../features/currencyFilter/currencyFilter';
+import {currencyFilterReducer} from '../features/currencyFilter/currencyFilterSlice';
 import {inventoryReducer} from '../features/inventory/inventorySlice';
 
 // Create and export the store here.
