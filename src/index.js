ff433249f2178c021e39d1d7e1b51cce8c8acebf
mikejguo290 <mikejguo290@gmail.com>
2021-07-31 17:33:32 +0100

    having completed the actions, reducers and store, now pass this data to view to render.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { App } from './app/App.js';
+
+// having completed the actions, reducers and store, now pass this data to view to render.
 // Import the store here.
 
 // Pass state and dispatch props to the <App /> component.
