7572bb02f3d80d13f4aa16731a5e6b7aa9da281b
mikejguo290 <mikejguo290@gmail.com>
2021-07-31 17:15:46 +0100

    import three available reducers into store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,9 @@
 // Import createStore and combineReducers here.
 import { createStore,  combineReducers } from "redux";
 // Import the slice reducers here.
+import {cartReducer} from '../features/cart/cartSlice';
+import {currencyFilterReducer} from '../features/currencyFilter/currencyFilter';
+import {inventoryReducer} from '../features/inventory/inventorySlice';
 
 // Create and export the store here.
+
