e8fbbc5bdb47b9373cb241b6fbe331963ff78054
mikejguo290 <mikejguo290@gmail.com>
2021-07-31 17:45:03 +0100

    import the store in index.js - entry point of the app.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { App } from './app/App.js';
 
 // having completed the actions, reducers and store, now pass this data to view to render.
 // Import the store here.
+import { store } from './app/store.js';
 
 // Pass state and dispatch props to the <App /> component.
 const render = () => {
