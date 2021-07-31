2c034b242d27174ee17e22f6ad16995756af5003
mikejguo290 <mikejguo290@gmail.com>
2021-07-31 18:52:44 +0100

    start to render Cart in App by importing it from features/cart/Cart.js.

--- src/app/App.js
+++ src/app/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Inventory } from '../features/inventory/Inventory.js';
 import { CurrencyFilter } from '../features/currencyFilter/CurrencyFilter.js';
 // Import the Cart component here.
-
+import { Cart } from '../features/cart/Cart.js'
 // Render the Cart component below <Inventory />
 export const App = (props) => {
 
