82601e8856ca26a431bf635b8bdb661367d36d95
mikejguo290 <mikejguo290@gmail.com>
2021-08-02 11:50:44 +0100

    import searchTermReducer from searchTermSlice and add it to reducers obj in combineReducers(reducers);

--- src/app/store.js
+++ src/app/store.js
@@ -4,13 +4,15 @@ import { createStore,  combineReducers } from "redux";
 import {cartReducer} from '../features/cart/cartSlice';
 import {currencyFilterReducer} from '../features/currencyFilter/currencyFilterSlice';
 import {inventoryReducer} from '../features/inventory/inventorySlice';
+import {searchTermReducer} from '../features/searchTerm/searchTermSlice';
 
 // Create and export the store here.
 const reducers = {
-    // like a blue print for the state obj returned by combineReducers. with namespacing each state to the keys set here. 
+    // like a blue print for the state obj eventually returned by createStore(rootReducer). with namespacing each state to the keys set here. 
     cart: cartReducer,
     currencyFilter: currencyFilterReducer,
     inventory: inventoryReducer,
+    searchTerm: searchTermReducer,
 }
 
 const rootReducer = combineReducers(reducers);
